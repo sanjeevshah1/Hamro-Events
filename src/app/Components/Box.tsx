fa0dc504a29cdc4fb229e97817ca8d57b88ca9a4
sanjeevshah1 <sahsanjeev42@gmail.com>
2025-01-06 19:37:37 +0545

    refined box

--- src/app/Components/Box.tsx
+++ src/app/Components/Box.tsx
@@ -3,13 +3,13 @@
 
 const Box = ({event, date, venue}:{event:string, date:Date, venue:string}) => {
   return (
-    <div className="bg-[#D9D9D9] font-josefinSlab flex flex-row gap-2 p-2 rounded-md">
+    <div className="bg-white/60  h-24 font-josefinSlab flex flex-row gap-2 p-2 rounded-md  hover:-translate-y-1 hover:shadow-lg transition-transform duration-300 my-2">
         <img className="w-20 rounded-md my-auto" src="camera.jpg" alt="Anything"/>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col  justify-center w-full">
             <p>{event}</p>
             <div className="flex gap-2 ">
-                <p className="">Date:{date.toLocaleDateString()}</p>
-                <p className="">Venue:{venue}</p>
+                <p className="">Date: {date.toLocaleDateString()}</p>
+                <p className="">Venue: {venue}</p>
             </div>
         </div>
     </div>
