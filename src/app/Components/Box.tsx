15b534bd9067ffd4b3c9b33260f1502f3c300aed
Nischal-Ojha <nischalojha59@gmail.com>
2025-01-06 20:20:25 +0545

    Change in box

--- src/app/Components/Box.tsx
+++ src/app/Components/Box.tsx
@@ -1,6 +1,6 @@
 const Box = ({event, date, venue}:{event:string, date:Date, venue:string}) => {
   return (
-    <div className="bg-white/60  h-24 font-josefinSlab flex flex-row gap-2 p-2 rounded-md  hover:-translate-y-1 hover:shadow-lg transition-transform duration-300 my-2">
+    <div className="bg-white/60 h-24 font-josefinSlab flex flex-row gap-2 p-2 rounded-md  hover:-translate-y-1 hover:shadow-lg transition-transform duration-300 my-2">
         <img className="w-20 rounded-md my-auto" src="camera.jpg" alt="Anything"/>
         <div className="flex flex-col  justify-center w-full">
             <p>{event}</p>
