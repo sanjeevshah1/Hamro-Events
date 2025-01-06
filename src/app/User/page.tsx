b4bc0d0d32ebce1359c52ad0149784952f22384e
Nischal-Ojha <nischalojha59@gmail.com>
2025-01-06 16:48:39 +0545

    User page

---
+++ src/app/User/page.tsx
@@ -0,0 +1,11 @@
+const user = () => {
+  return (
+    <div className="bg-mainColor min-h-screen font-josefinSlabn flex flex-col items-center">
+      <div className="m-4 p-4">
+        <img src="avatar.png" alt="Image of User." />
+      </div>
+    </div>
+  )
+}
+
+export default user
